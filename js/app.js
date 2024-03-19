ef54fae6d081161c32f8dbf7cc26ad5f44c225bc
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-19 14:39:10 +0100

    ADD: created tow options of work

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-console.log('ciao mamma')
+// console.log('ciao mamma')
 
 
 /*
+
 Dwayne Johnson ci ha chiesto di creare i segnaposto per il tavolo degli invitati alla sua mega festa vip.
 Ci ha lasciato il nome del tavolo (“Tavolo Vip”) e la lista degli invitati in ordine di posto:
 [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin']
@@ -10,9 +11,67 @@ nome del tavolo,
 nome dell’ospite,
 posto occupato.
 Generiamo e stampiamo in console la lista per i segnaposto.
+
 */
 
-const MemberName = [ 
+// const MembersName = [ 
+//     {
+//         nomeTavolo: 'Tavolo Vip',
+//         nomeOspite:'Brad Pitt',
+//         posto: ''
+
+//     },
+//     {
+//         nomeTavolo: 'Tavolo Vip',
+//         nomeOspite:'Johnny Depp',
+//         posto: ''
+
+//     },
+//     {
+//         nomeTavolo: 'Tavolo Vip',
+//         nomeOspite:'Lady Gaga', 
+//         posto: ''
+//     },
+//     {
+//         nomeTavolo: 'Tavolo Vip',
+//         nomeOspite:'Cristiano Ronaldo', 
+//         posto: ''
+//     },
+//     {
+//         nomeTavolo: 'Tavolo Vip',
+//         nomeOspite:'Georgina Rodriguez', 
+//         posto: ''
+//     },
+//     {
+//         nomeTavolo: 'Tavolo Vip',
+//         nomeOspite:'Chiara Ferragni', 
+//         posto: ''
+
+//     },
+//     {
+//         nomeTavolo: 'Tavolo Vip',
+//         nomeOspite: 'Fedez', 
+//         posto: ''
+//     },
+//     {
+//         nomeTavolo: 'Tavolo Vip',
+//         nomeOspite: 'George Clooney',
+//         posto: '' 
+//     },
+//     {
+//         nomeTavolo: 'Tavolo Vip',
+//         nomeOspite: 'Amal Clooney',
+//         posto: '' 
+//     },
+//     {
+//         nomeTavolo: 'Tavolo Vip',
+//         nomeOspite: 'Maneskin',
+//         posto: '' 
+//     },
+     
+// ];
+
+const MembersName = [ 
     'Brad Pitt', 
     'Johnny Depp', 
     'Lady Gaga', 
@@ -20,7 +79,12 @@ const MemberName = [
     'Georgina Rodriguez', 
     'Chiara Ferragni', 
     'Fedez', 
-    'George Clooney',
-    'Amal Clooney',
+    'George Clooney', 
+    'Amal Clooney', 
     'Maneskin'
 ];
+
+for(let i = 0; i < MembersName.length; i++){
+    const singleMember = MembersName[i]
+    //console.log(singleMember);
+}
