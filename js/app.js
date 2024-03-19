d003f1c2c166f7227cf141f465545d9643793dc8
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-19 14:21:09 +0100

    ADD: created the array

---
+++ js/app.js
@@ -0,0 +1,26 @@
+console.log('ciao mamma')
+
+
+/*
+Dwayne Johnson ci ha chiesto di creare i segnaposto per il tavolo degli invitati alla sua mega festa vip.
+Ci ha lasciato il nome del tavolo (“Tavolo Vip”) e la lista degli invitati in ordine di posto:
+[ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin']
+Ma la tipografia per stampare il tutto vuole che le mandiamo una lista di ospiti in un formato specifico, per cui dobbiamo fare in modo che ogni ospite sia un oggetto javascript che ha come attributi:
+nome del tavolo,
+nome dell’ospite,
+posto occupato.
+Generiamo e stampiamo in console la lista per i segnaposto.
+*/
+
+const MemberName = [ 
+    'Brad Pitt', 
+    'Johnny Depp', 
+    'Lady Gaga', 
+    'Cristiano Ronaldo', 
+    'Georgina Rodriguez', 
+    'Chiara Ferragni', 
+    'Fedez', 
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin'
+];
